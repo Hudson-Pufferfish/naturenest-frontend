6de67a05e80314a2941689062c958670e70de694
Hudson Nguyen <hungson.nguyen811@gmail.com>
2024-11-21 19:15:41 -0600

    chore(auth): added middleware file and utils link file

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,6 +1,6 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
 import { LuAlignLeft } from "react-icons/lu";
-// import Link from "next/link";
+import Link from "next/link";
 import { Button } from "../ui/button";
 import UserIcon from "./UserIcon";
 // import { links } from "@/utils/links";
---
+++ utils/links.ts
@@ -0,0 +1,16 @@
+type NavLink = {
+  href: string;
+  label: string;
+};
+
+export const links: NavLink[] = [
+  { href: "/", label: "home" },
+  { href: "/favorites ", label: "favorites" },
+  { href: "/bookings ", label: "bookings" },
+  { href: "/reviews ", label: "reviews" },
+  { href: "/reservations ", label: "reservations" },
+  { href: "/properties/create ", label: "create property" },
+  { href: "/properties", label: "my properties" },
+  { href: "/admin", label: "admin" },
+  { href: "/profile ", label: "profile" },
+];
