a2dcf1f4d223277a49447bc2729d5162c93e853b
Hudson Nguyen <96578906+Hudson-Pufferfish@users.noreply.github.com>
2024-12-06 17:21:21 -0600

    chore(property): add countryCode (#10)

--- types/country.ts
+++
@@ -1,7 +0,0 @@
-export interface Country {
-  id: string; // Unique identifier
-  name: string; // Name of the country
-  code: string; // Country code (ISO)
-  createdAt: Date; // Creation timestamp
-  updatedAt: Date; // Last updated timestamp
-}
