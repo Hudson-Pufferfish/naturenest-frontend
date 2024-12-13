942abd0d9cdb646c627533a9140c4720cbad8504
Hudson Nguyen <hungson.nguyen811@gmail.com>
2024-12-13 10:44:42 -0600

    add reservation creation page

--- components/form/CounterInput.tsx
+++ components/form/CounterInput.tsx
@@ -5,26 +5,29 @@ import { LuMinus, LuPlus } from "react-icons/lu";
 import { Button } from "../ui/button";
 import { useState } from "react";
 
-function CounterInput({ detail, defaultValue }: { detail: string; defaultValue?: number }) {
-  const [count, setCount] = useState(defaultValue || 0);
+interface CounterInputProps {
+  detail: string;
+  defaultValue?: number;
+  max?: number;
+}
 
-  const increaseCount = () => {
-    setCount((prevCount) => prevCount + 1);
+function CounterInput({ detail, defaultValue = 1, max }: CounterInputProps) {
+  const [value, setValue] = useState(defaultValue);
+
+  const increment = () => {
+    if (max && value >= max) return;
+    setValue(value + 1);
   };
 
-  const decreaseCount = () => {
-    setCount((prevCount) => {
-      if (prevCount > 0) {
-        return prevCount - 1;
-      }
-      return prevCount;
-    });
+  const decrement = () => {
+    if (value > 1) {
+      setValue(value - 1);
+    }
   };
 
   return (
     <Card className="mb-4">
-      {/* input */}
-      <input type="hidden" name={detail} value={count} />
+      <input type="hidden" name={detail} value={value} />
       <CardHeader className="flex flex-col gap-y-5">
         <div className="flex items-center justify-between flex-wrap">
           <div className="flex flex-col">
@@ -32,11 +35,11 @@ function CounterInput({ detail, defaultValue }: { detail: string; defaultValue?:
             <p className="text-muted-foreground text-sm">Specify the number of {detail}</p>
           </div>
           <div className="flex items-center gap-4">
-            <Button variant="outline" size="icon" type="button" onClick={decreaseCount}>
+            <Button variant="outline" size="icon" type="button" onClick={decrement}>
               <LuMinus className="w-5 h-5 text-primary" />
             </Button>
-            <span className="text-xl font-bold w-5 text-center">{count}</span>
-            <Button variant="outline" size="icon" type="button" onClick={increaseCount}>
+            <span className="text-xl font-bold w-5 text-center">{value}</span>
+            <Button variant="outline" size="icon" type="button" onClick={increment} disabled={max ? value >= max : false}>
               <LuPlus className="w-5 h-5 text-primary" />
             </Button>
           </div>
